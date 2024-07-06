9886ebbaaecac6d6993ad8f7821706a9c48e70e3
ijoumiyamura <147089880+ijoumiyamura@users.noreply.github.com>
2024-07-06 22:57:23 +0800

    relay

--- Assets/Scripts/GameLobby.cs
+++ Assets/Scripts/GameLobby.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.Animations;
 
 public class GameLobby : MonoBehaviour
 {
+    private const string KEY_RELAY_JOIN_CODE = "RelayJoinCode";
     public static GameLobby Instance { get; private set; }
 
     private Lobby joinedLobby;
@@ -59,6 +66,18 @@ public class GameLobby : MonoBehaviour
                 IsPrivate = isPrivate,
             });
 
+            Allocation allocation = await AllocateRelay();
+
+            string relayJoinCode = await GetRelayJoinCode(allocation);
+
+            await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions{
+                Data = new Dictionary<string, DataObject>{
+                    { KEY_RELAY_JOIN_CODE, new DataObject(DataObject.VisibilityOptions.Member, relayJoinCode) }
+                }
+            });
+            
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+            
             GameMultiplayer.Instance.StartHost();
         } catch (LobbyServiceException e){
             Debug.Log(e);
@@ -68,9 +87,51 @@ public class GameLobby : MonoBehaviour
     public async void QuickJoin(){
         try {
             joinedLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+
+            string relayJoinCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value;
+            JoinAllocation joinAllocation = await JoinRelay(relayJoinCode);
+            
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+            
             GameMultiplayer.Instance.StartClient();
         } catch (LobbyServiceException e){
             Debug.Log(e);
+        }
+    }
+
+    private async Task<Allocation> AllocateRelay(){
+        try{
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(9);
+
+            return allocation;
+        } catch (RelayServiceException e){
+            Debug.Log(e);
+
+            return default;
+        }
+    }
+
+    private async Task<string> GetRelayJoinCode(Allocation allocation){
+        try{
+            string relayJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            return relayJoinCode;
+        } catch (RelayServiceException e){
+            Debug.Log(e);
+
+            return default;
+        }
+    }
+
+    private async Task<JoinAllocation> JoinRelay(string joinCode){
+        try{
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            return joinAllocation;
+        } catch (RelayServiceException e){
+            Debug.Log(e);
+
+            return default;
         }
     }
 }
